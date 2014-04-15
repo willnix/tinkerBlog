f21963f89e5ec332614cb46a52d5d9a8166c1200
Frieder Steinmetz <frieder.steinmetz@gmail.com>
2014-04-15 17:05:01 +0200

    Initial commit

---
+++ handlers/blog.go
@@ -0,0 +1,52 @@
+package blog
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"strconv"
+	"time"
+)
+
+// struct for dbquery results
+// mgo requires the string literal tags
+type dbBlogEntry struct {
+	ObjId   bson.ObjectId "_id,omitempty"
+	Id      int           "id"
+	Title   string        "title"
+	Author  string        "author"
+	Text    string        "text"
+	Written time.Time     "written"
+}
+
+func BlogEntryList(ren render.Render, db *mgo.Database) {
+	var results []dbBlogEntry
+
+	// Load all Blogentries in the results slice
+	// (sorted descending according to id)
+	db.C("blogEntries").Find(nil).Sort("-id").All(&results)
+
+	// render the template using the results from the db
+	ren.HTML(200, "blogEntryList", results)
+}
+
+func BlogEntry(ren render.Render, db *mgo.Database, args martini.Params) {
+	var result dbBlogEntry
+
+	Id, _ := strconv.Atoi(args["Id"])
+
+	// Find Blogentry by Id (should be only one)
+	db.C("blogEntries").Find(bson.M{"id": Id}).One(&result)
+
+	// render the template using the result from the db
+	ren.HTML(200, "blogEntry", result)
+}
+
+func About(ren render.Render) {
+	ren.HTML(200, "about", nil)
+}
+
+func Impressum(ren render.Render) {
+	ren.HTML(200, "impressum", nil)
+}
---
+++ site.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"github.com/willnix/blog/handlers"
+	"html/template"
+	"labix.org/v2/mgo"
+	"time"
+)
+
+// Middleware handler for mongodb
+func Mongo() martini.Handler {
+	session, err := mgo.Dial("localhost/blog")
+	if err != nil {
+		panic(err)
+	}
+
+	return func(c martini.Context) {
+		reqSession := session.Clone()
+		c.Map(reqSession.DB("blog"))
+		defer reqSession.Close()
+
+		c.Next()
+	}
+}
+
+func main() {
+	m := martini.Classic()
+
+	//needs import ("time")
+	m.Use(render.Renderer(render.Options{
+		Directory: "templates",
+		Layout:    "layout",
+		Funcs: []template.FuncMap{
+			{
+				"formatTime": func(args ...interface{}) string {
+					t1 := time.Time(args[0].(time.Time))
+					return t1.Format("Jan 2, 2006 at 3:04pm (MST)")
+				},
+				"unescaped": func(args ...interface{}) template.HTML {
+					return template.HTML(args[0].(string))
+				},
+			},
+		},
+	}))
+
+	// Middleware for mongodb connection
+	m.Use(Mongo())
+
+	// Setup static file serving
+	m.Use(martini.Static("assets"))
+
+	// Setup routing
+	m.Get("/", blog.BlogEntryList)
+	m.Get("/post/:Id", blog.BlogEntry)
+	m.Get("/about", blog.About)
+	m.Get("/impressum", blog.Impressum)
+
+	m.Run()
+}
